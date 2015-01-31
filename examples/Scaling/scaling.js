bd05a7242e557378b9527ab33c047a7b15161df8
Ralph Thomas <ralpht@gmail.com>
2015-01-30 18:38:33 -0800

    Switch scaling example to JSON form, much better.

--- examples/Scaling/scaling.js
+++ examples/Scaling/scaling.js
@@ -1,65 +1,52 @@
 "use strict";
 
+var parentWidth = 320;
+var parentHeight = 480;
+var aspect = parentWidth / parentHeight;
+
+var scalingExample = {
+    box: {
+        id: "scaleBox",
+        className: "box",
+        // Specify a layoutWidth and layoutHeight. This means that the box will
+        // use a transform to get its correct width and height from the constraint
+        // solver and will have these values as it's DOM Layout dimensions. We do
+        // this because updating a transform is faster than causing a DOM relayout.
+        layoutWidth: parentWidth,
+        layoutHeight: parentHeight
+    },
+    constraints: [
+        // Scale is initially 0.45
+        "scale == 0.45",
+        // Define some aliases for width, height and horizontal center.
+        "width == scaleBox.right - scaleBox.left",
+        "height == scaleBox.bottom - scaleBox.top",
+        "centerX == scaleBox.left + width * 0.5",
+        // We want the aspect ratio preserved, so width == height * aspect
+        "width == height * " + aspect,
+        // Relate the height and the scale. We define a scale of 1.0 to be the parent height.
+        "height == scale * " + parentHeight,
+        // Pin the bottom of the box to the bottom of the parent.
+        "scaleBox.bottom == " + parentHeight,
+        // Center the box horizontally.
+        "centerX == " + (parentWidth / 2)
+    ],
+    motionConstraints: [
+        // Constrain the scale with springs. We could constrain the top or something else
+        // and backtracking would ensure that we got the same effect.
+        { lhs: "scale", op: ">=", rhs: 0.45 },
+        { lhs: "scale", op: "<=", rhs: 1 }
+    ],
+    manipulators: [
+        // Dragging in "y" should move the top of the scaleBox.
+        { variable: "scaleBox.top", box: "scaleBox", axis: "y" }
+    ]
+};
+
 function makeScalingExample(parentElement) {
-    var parentHeight = 480;
-    var parentWidth = 320;
-
-    var context = new Slalom.MotionContext();
-    var solver = context.solver();
-
-    var scale = new c.Variable({name: 'scale'});
-
-    var box = new Slalom.Box('');
-
-    box.x = new c.Variable({name: 'x'});
-    box.y = new c.Variable({name: 'y'});
-    box.bottom = new c.Variable({name: 'bottom'});
-    box.right = new c.Variable({name: 'right'});
-
-    // Set these DOM layout properties on Box so that it'll use a CSS
-    // transform to apply the scale.
-    box.domWidth = parentWidth;
-    box.domHeight = parentHeight;
-
-    var width = c.minus(box.right, box.x);
-    var height = c.minus(box.bottom, box.y);
-
-    // The width and height are related: we must keep the aspect ratio.
-    //
-    //   width = height * aspect
-    //
-    var aspect = parentWidth / parentHeight;
-    solver.add(eq(width, c.times(height, aspect), required));
-
-    // The height is controlled by the scale.
-    //
-    //  height = scale * 480 (parentHeight)
-    //
-    solver.add(eq(height, c.times(scale, parentHeight), medium));
-
-    // The bottom of the box is pinned to the bottom of the screen, like FB Paper.
-    //
-    //  bottom = 480 (parentHeight)
-    //
-    solver.add(eq(parentHeight, box.bottom, medium));
-
-    // The box is centered horizontally.
-    // 
-    //  centerX := x + width/2
-    //  centerX = 320/2 (parentWidth / 2)
-    //
-    var centerX = c.plus(box.x, c.times(width, 0.5));
-    solver.add(eq(centerX, parentWidth/2, medium));
-
-    // Motion constraints on scale. We express the constraints on other variables.
-    // Use a physics model that doesn't overbounce.
-    context.addMotionConstraint(new Slalom.MotionConstraint(scale, '>=', 0.45, { physicsModel: Slalom.MotionConstraint.criticallyDamped }));
-    context.addMotionConstraint(new Slalom.MotionConstraint(scale, '<=', 1, { physicsModel: Slalom.MotionConstraint.criticallyDamped }));
-    solver.add(eq(scale, 0.45, weak));
-
-    parentElement.appendChild(box.element());
-    context.addBox(box);
-    context.addManipulator(createManipulator(box.y, parentElement, 'y'));
+    var context = Slalom.Serialization.assemble(scalingExample);
+    parentElement.appendChild(context.boxes()[0].element());
 }
+        
 makeScalingExample(document.getElementById('scaling-example'));
 
