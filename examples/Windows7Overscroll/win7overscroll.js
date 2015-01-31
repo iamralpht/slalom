2807f3c985f585602d5a6e9556caeda79d52a6ec
Ralph Thomas <ralpht@gmail.com>
2015-01-30 21:04:31 -0800

    Convert the Windows Overscroll example over to the new declarative style.
    Interestingly I haven't needed any strengths on the linear constraints yet. I know that I will need some (the twitter panels example particularly needs them) but none of the examples I've converted over so far did.

--- examples/Windows7Overscroll/win7overscroll.js
+++ examples/Windows7Overscroll/win7overscroll.js
@@ -17,70 +17,80 @@ limitations under the License.
 */
 
 // Windows 7-style window-based overscrolling
-function makeWindows7Overscroll(parentElement) {
-    var parentHeight = 550;
-    var windowWidth = 470;
-    var windowHeight = 427;
 
-    var contentWidth = 450;//427;
-    var contentHeight = (640/427) * 450;
+// These "magic" numbers come from the image assets that this demo is built with.
+// There's nothing special about them.
+var parentHeight = 550;
+var windowWidth = 470;
+var windowHeight = 427;
 
-    var windowTop = (parentHeight - windowHeight) / 2;
+var contentWidth = 450;//427;
+var contentHeight = (640/427) * 450;
 
-    var contentArea = { x: 10, y: 47, width: 450, height: 370 };
+var windowTop = (parentHeight - windowHeight) / 2;
+var contentArea = { x: 10, y: 47, width: 450, height: 370 };
 
-    var context = new Slalom.MotionContext();
-    var solver = context.solver();
+var windowsOverscrollExample = {
+    // Outer box is the actual window.
+    box: {
+        id: "win",
+        className: "window",
+        children: [
+            {
+                // Inside the window is the clip.
+                id: "clip",
+                className: "clip",
+                children: [
+                    {
+                        // Inside the clip is the content, which is an image.
+                        id: "content",
+                        className: "image"
+                    }
+                ]
+            }
+        ]
+    },
+    constraints: [
+        // Position the outer window. All of the x values (left, right) in this example are static.
+        "win.left == 0",
+        "win.right == " + windowWidth,
+        "win.top == " + windowTop,
+        "win.bottom == win.top + " + windowHeight,
 
-    var win = new Slalom.Box(parentElement.querySelector('.window'));
-    var clip = new Slalom.Box(parentElement.querySelector('.clip'));
-    var content = new Slalom.Box(parentElement.querySelector('.image'));
+        // Position the clipping layer. It just occupies the "content area" of the window and has
+        // an "overflow: hidden" style applied.
+        "clip.left == " + contentArea.x,
+        "clip.right == " + (contentArea.x + contentArea.width),
+        "clip.top == win.top + " + contentArea.y,
+        "clip.bottom == win.top + " + (contentArea.y + contentArea.height),
 
-    context.addBox(win);
-
-    win.addChild(clip);
-    clip.addChild(content);
-
-    // Describe the window size. This can't change (in this example) so we use
-    // strong constraints. If I was using border-image or something for the window
-    // boundary then I *could* make it resizable, which would be pretty awesome.
-    win.x = 0;
-    win.right = windowWidth;
-    win.y = new c.Variable();
-    win.bottom = new c.Variable();
-    var winHeight = c.minus(win.bottom, win.y);
-
-    solver.add(eq(winHeight, windowHeight, strong));
-    solver.add(eq(win.y, windowTop, weak));
-
-    // Position the clip. This is just so that the content doesn't draw all over
-    // the window border.
-    clip.x = contentArea.x; clip.right = contentArea.x + contentArea.width;
-    clip.y = new c.Variable();
-    clip.bottom = new c.Variable();
-
-    // The clip is glued to the window.
-    solver.add(eq(clip.y, c.plus(win.y, contentArea.y), strong));
-    solver.add(eq(clip.bottom, c.plus(win.y, contentArea.y + contentArea.height), strong));
-
-    content.x = contentArea.x; content.right = contentArea.x + contentWidth;
-    content.y = new c.Variable(); content.bottom = new c.Variable();
-
-    // The content is always the same height.
-    solver.add(eq(content.bottom, c.plus(content.y, contentHeight), strong));
-    // Constrain the content to be inside the window -- this means that in order to overdrag
-    // the image, we'll have to move the whole window since its position is only weakly
-    // constrained.
-    solver.add(leq(content.y, clip.y, strong));
-    solver.add(geq(content.bottom, clip.bottom, strong));
-
-    // Motion constraints are really simple. We just want the window to stay put, enforced by a spring...
-    context.addMotionConstraint(new Slalom.MotionConstraint(win.y, '==', windowTop, { physicsModel: Slalom.MotionConstraint.criticallyDamped }));
-
-    // Manipulate the content's y.
-    context.addManipulator(createManipulator(content.y, parentElement, 'y'));
-
-    context.update();
+        // Position the content, the image.
+        "content.left == " + contentArea.x,
+        "content.right == " + (contentArea.x + contentArea.width),
+        // The image can't go past the top of the clip, this is enforced with a linear constraint
+        // so the clip will move if the content is moved (and the window will move too, because it's
+        // also attached).
+        "content.top <= clip.top",
+        // The image can't go past the clip's bottom; the clip will move so that the image's bottom
+        // and the clip's bottom stay touching.
+        "content.bottom >= clip.bottom",
+        // Specify the height of the image.
+        "content.bottom == content.top + " + contentHeight,
+        // Specify a default start position for the image.
+        "content.top == " + (contentArea.y + windowTop)
+    ],
+    motionConstraints: [
+        // Our only motion constraint is that the window's top stays where we started it.
+        { lhs: "win.top", op: "==", rhs: windowTop }
+    ],
+    manipulators: [
+        // The manipulator moves the image's top up and down.
+        { variable: "content.top", box: "win", axis: "y" }
+    ]
 }
 
-makeWindows7Overscroll(document.getElementById('win7-overscroll-example'));
+function makeDeclarativeWindows7Overscroll(parentElement) {
+    Slalom.Serialization.assemble(windowsOverscrollExample, parentElement);
+}
+
+makeDeclarativeWindows7Overscroll(document.getElementById('win7-overscroll-example'));
