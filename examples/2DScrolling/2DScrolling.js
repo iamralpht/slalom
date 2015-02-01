56479617df7e14cd25992d1d077f4ed0f772ef89
Ralph Thomas <ralpht@gmail.com>
2015-01-31 18:30:23 -0800

    Improve commentary for 2d scrolling example.

--- examples/2DScrolling/2DScrolling.js
+++ examples/2DScrolling/2DScrolling.js
@@ -1,4 +1,6 @@
 var scrollingExample = {
+    // Create two nested boxes. The outer box is a clip, and the inner box is an image to
+    // pan around.
     box: {
         id: "clip",
         className: "clip",
@@ -8,11 +10,14 @@ var scrollingExample = {
         }
     },
     constraints: [
+        // The clip is going to be 300x300.
         "clip.left == 0",
         "clip.top == 0",
         "clip.right == 300",
         "clip.bottom == 300",
-        // Specify the bounds of the image and hint its position.
+        // Specify the bounds of the image and hint its position; the image is 800x800.
+        // We don't really need to specify the constraint strengths here, but it helps
+        // to make clearer which constraints we expect to be violated.
         "image.right == image.left + 800 !strong",
         "image.bottom == image.top + 800 !strong",
         "image.left == 0 !weak",
@@ -20,13 +25,15 @@ var scrollingExample = {
     ],
     motionConstraints: [
         // Ensure that the image stays within the clip with springs.
-        // XXX: We can't yet express motion constraints relative to other linear variables.
-        [ "image.left", "<=", 0 ],//"clip.left" ],
-        [ "image.top", "<=", 0 ],//"clip.top" ],
-        [ "image.bottom", ">=", 300],//"clip.bottom" ],
-        [ "image.right", ">=", 300]//"clip.right" ],
+        // XXX: We can't yet express motion constraints relative to other linear variables; would rather use clip.right instead of "300" here.
+        [ "image.left", "<=", 0 ],
+        [ "image.top", "<=", 0 ],
+        [ "image.bottom", ">=", 300],
+        [ "image.right", ">=", 300]
     ],
     manipulators: [
+        // Create a manipulator that listens for events on the box "clip" and manipulates
+        // "image.left" and "image.top" for the x and y components of a pan.
         { box: "clip", x: "image.left", y: "image.top" }
     ]
 }
